da5d2130a80354df2d92a2269bc720d3dd304d8d
Jeff Tao <jatao88@gmail.com>
2026-03-17 23:47:21 -0400

    fix: commit note un-codings

--- coding/__init__.py
+++ coding/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, g
 
 
 def create_app() -> Flask:
@@ -8,5 +8,13 @@ def create_app() -> Flask:
 
     app.register_blueprint(web_bp)
     app.jinja_env.filters["ts"] = _seconds_to_ts
+
+    @app.teardown_appcontext
+    def close_db(exc):
+        db = g.pop("db", None)
+        if db is not None:
+            if exc is None:
+                db.conn.commit()
+            db.conn.close()
 
     return app
